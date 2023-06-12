84c9ba28f27b35b47af4f2e8be388c64256b0898
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-06-12 13:18:06 +0100

    Feat: Finished the app-header-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import "./App.css";
 import "./autoload";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Download from "./pages/Download";
+import "./App.css";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,39 +1,76 @@
 import React from "react";
+import LogoText from "./../assets/logo-text.png";
+
 import { Link } from "react-router-dom";
-import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
-
 const AppHeader: React.FC = (): JSX.Element => {
-  // const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
-
   return (
     <React.Fragment>
-      <Nav variant="pills" activeKey="1">
-        <Nav.Item>
-          <Nav.Link eventKey="1" href="#/home">
-            NavLink 1 content
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="2" title="Item">
-            NavLink 2 content
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="3" disabled>
-            NavLink 3 content
-          </Nav.Link>
-        </Nav.Item>
-        <NavDropdown title="Dropdown" id="nav-dropdown">
-          <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
-          <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item eventKey="4.3">
-            Something else here
-          </NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
+      <nav className={`navbar navbar-expand-lg`} aria-label="Fotograph Header">
+        <div className="container-fluid">
+          <img
+            src={LogoText}
+            className="img-fluid"
+            height="30"
+            width="170"
+            alt="site-logo"
+          />
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#Fotograph"
+            aria-controls="Fotograph"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 24 24"
+              className="text-light"
+              height="35"
+              width="35"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M4 11h12v2H4zm0-5h16v2H4zm0 12h7.235v-2H4z"></path>
+            </svg>
+          </button>
+
+          <div className="collapse navbar-collapse" id="Fotograph">
+            <ul className="navbar-nav">
+              <li className="nav-item mx-md-5 mx-4 my-md-0 my-3">
+                <Link
+                  aria-current="page"
+                  className="text-decoration-none brand-white-text text-capitalize"
+                  to="/home"
+                >
+                  home
+                </Link>
+              </li>
+
+              <li className="nav-item mx-md-5 mx-4 my-md-0 my-3">
+                <Link
+                  to="/about"
+                  className="text-decoration-none brand-white-text text-capitalize"
+                >
+                  about
+                </Link>
+              </li>
+
+              <li className="nav-item mx-md-5 mx-4 my-md-0 my-3">
+                <Link
+                  to="/support"
+                  className="text-decoration-none brand-white-text text-capitalize"
+                >
+                  support
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </React.Fragment>
   );
 };
