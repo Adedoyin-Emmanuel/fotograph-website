75af509248bcab7cc54e41119e35df2003754230
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-06-12 15:21:00 +0100

    Feat: Updated the header and hero component

--- src/components/AppHeroSection1.tsx
+++ src/components/AppHeroSection1.tsx
@@ -1,18 +1,26 @@
-
 import React from "react";
+import { Container } from "react-bootstrap";
 
-interface AppHeroSection1Props {
-    
-}
+const AppHeroSection1: React.FC = (): JSX.Element => {
+  return (
+    <React.Fragment>
+      <Container fluid className="m-auto py-4">
+        <h1 className=" display-3 text-center text-capitalize brand-heading-text fw-bold my-3">
+          A tool for image manipulation
+        </h1>
 
-const AppHeroSection1:React.FC = (): JSX.Element => {
-
-    return (
-        <React.Fragment>
-            <h1>AppHeroSection1 works!</h1>
-        </React.Fragment>
-    );  
-}
+        <section className="text-container m-auto width-toggle  text-center d-flex align-items-center justify-content-center">
+          <p className="text-center brand-text-muted brand-small-text-3 my-4">
+            FotoGraph brings most image manipulation tools we use online to a
+            single offline application.
+          </p>
+        </section>
+        
+        
+        
+      </Container>
+    </React.Fragment>
+  );
+};
 
 export default AppHeroSection1;
-    
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AppHeader from "../components/AppHeader";
-interface HomeProps {}
+import AppHeroSection1 from "../components/AppHeroSection1";
+
 
 const Home: React.FC = (): JSX.Element => {
   return (
     <React.Fragment>
       <section className="container-fluid">
         <AppHeader></AppHeader>
+        <AppHeroSection1></AppHeroSection1>
       </section>
     </React.Fragment>
   );
